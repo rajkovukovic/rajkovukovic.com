0b4efa6f0be489da1be21af6dbcd1f190a618b75
Rajko Vukovic <rajkovukovic@gmail.com>
2025-01-06 20:48:36 +0000

    add apter-static

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,8 @@
-import { redirect } from '@sveltejs/kit';
 
-export function load({ url }) {
-	if (url.pathname === '/') {
-		redirect(307, '/password-generator');
-	}
+export const prerender = true;
+
+export function load() {
+	// if (url.pathname === '/') {
+	// 	redirect(307, '/password-generator');
+	// }
 }
